20591f4efd4df9bdbc1c0d71cc6b701a084f8c29
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-07-09 17:38:48 +0100

    ⚙ changes made to pageExtenstions in next-config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['.jsx', '.js', '.css'],
+  pageExtensions: ['.'],
 }
 
 module.exports = nextConfig
