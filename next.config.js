7859f1b3259a31f1893de885592ec872a7410e0b
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-07-09 17:30:55 +0100

    🧪 running final test

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['mdx', 'md', 'tsx', 'ts', 'jsx', 'js'],
+  pageExtensions: ['.tsx', '.ts', '.jsx', '.js'],
 }
 
 module.exports = nextConfig
