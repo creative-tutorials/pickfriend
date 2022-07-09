da0919922338567edd0935aa55872dc1e0bff3a4
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-07-09 18:51:36 +0100

    ⬇️ returned next.config file to defaults

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  pageExtensions: ["jsx", "js"], //? add this to support .jsx files as well as .js files
 }
 
 module.exports = nextConfig
