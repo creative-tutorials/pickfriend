e39d14989b44df25448d851ea7a81994125854ed
Treasure <68476321+Treasure-dell@users.noreply.github.com>
2022-07-13 22:22:23 +0100

    🐛 Fixed a bug

--- pages/components/post/handlePost.jsx
+++ pages/components/post/handlePost.jsx
@@ -1,3 +1,21 @@
+import { doc, setDoc, deleteDoc, getFirestore } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 export function sendHandlePost(
   retrTextblock,
   imgObjURL,
@@ -8,12 +26,12 @@ export function sendHandlePost(
   const postid =
     Math.random().toString(36).substring(2, 15) +
     Math.random().toString(36).substring(2, 15);
-  const cityRef = doc(db, "ImagePost", postid);
+  const imagePostRef = doc(db, "ImagePost", postid);
   let post = {
     text: retrTextblock,
     image: imgObjURL,
   };
-  setDoc(cityRef, {
+  setDoc(imagePostRef, {
     data: post,
   });
   setisAccepted(true);
